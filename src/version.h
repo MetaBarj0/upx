730c6ce6c52158280cc7fc2e4ea73b6c38970eaa
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2025-05-06 12:19:16 +0200

    all: prepare for release

--- src/version.h
+++ src/version.h
@@ -2,6 +2,6 @@
 #define UPX_VERSION_HEX      0x050001 /* 05.00.01 */
 #define UPX_VERSION_STRING   "5.0.1"
 #define UPX_VERSION_STRING4  "5.01"
-#define UPX_VERSION_DATE     "Feb 21st 2025"
-#define UPX_VERSION_DATE_ISO "2025-02-21"
+#define UPX_VERSION_DATE     "May 6th 2025"
+#define UPX_VERSION_DATE_ISO "2025-05-06"
 #define UPX_VERSION_YEAR     "2025"
